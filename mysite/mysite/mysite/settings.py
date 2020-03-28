100be0bbb362906754e881fb5b120a620abedbf6
peterwaf <peterwafulah@gmail.com>
2020-03-28 10:31:24 +0300

    configuring the DB

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'localhost',
+        'PORT':3306,
+        'NAME': 'blog_app',
+        'USER':'root',
+        'PASSWORD':'pishpan88'
     }
 }
 

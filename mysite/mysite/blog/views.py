0e7bcc0724590e782e8c59b69d69032c8c4d6f91
peterwaf <peterwafulah@gmail.com>
2020-03-28 21:42:30 +0300

    adding views

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Post
 # Create your views here.
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/index.html'
+    
+class PostDeta(generic.ListView):
+    model = Post
+    template_name = 'blog/post_details.html'
